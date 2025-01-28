bd4d298aff0c71a32ca7cb0edab94c7b468bc832
Maximus301191 <maxalut301191@gmail.com>
2025-01-28 19:37:00 +0300

    final commit

--- src/BmiService.java
+++ src/BmiService.java
@@ -1,7 +1,7 @@
 public class BmiService {
     public int calculate(int weightKg, double hightM) //вес в кг и рост в метрах
     {
-        double result = hightM / Math.pow(weightKg, 2);
+        double result = weightKg / Math.pow(hightM, 2);
         return (int) result;
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         BmiService service = new BmiService();
-        double hightM = 1.97;
+        double hightM = 1.87;
         int weightKg = 98;
         int BMI = service.calculate(weightKg, hightM);
         System.out.println(BMI);
